a5d67ca08ffd6dcbf0ad255046d8b31188fe6de3
lvsenlv <lvsen46000@163.com>
2018-09-09 12:11:19 +0800

    Add libparser to lvsenlv_git.sh

--- lvsenlv_git.sh
+++ lvsenlv_git.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+RepositoryCount=10
 function echo_message()
 {
     echo "
     *** please input an available number ***
 
-    1. git clone git@github.com:lvsenlv/c_test.git
-    2. git clone git@github.com:lvsenlv/sources.git
+    1. git clone git@github.com:lvsenlv/sources.git
+    2. git clone git@github.com:lvsenlv/ubuntu_config.git
     3. git clone git@github.com:lvsenlv/vim_config.git
-    4. git clone git@github.com:lvsenlv/ubuntu_config.git
-    5. git clone git@github.com:lvsenlv/jz2440.git
-    6. git clone git@github.com:lvsenlv/graduation.git
-    7. git clone git@github.com:lvsenlv/c_projects.git
+    4. git clone git@github.com:lvsenlv/c_projects.git
+    5. git clone git@github.com:lvsenlv/c_test.git
+    6. git clone git@github.com:lvsenlv/eServer.git
+    7. git clone git@github.com:lvsenlv/easy_chat_server.git
     8. git clone git@github.com:lvsenlv/s3c2440a.git
+    9. git clone git@github.com:lvsenlv/encrypt.git
+    10. git clone git@github.com:lvsenlv/libparser.git
     "
     return 0
 }
@@ -23,35 +26,41 @@ if [ $? != 0 ];then
     exit 1
 fi
 
-if [[ -z $1 || $1 -lt 1 || $1 -gt 8 ]];then
+if [[ -z $1 || $1 -lt 1 || $1 -gt $RepositoryCount ]];then
     echo_message
     exit 1
 fi
 
 case $1 in 
     1 )
-        git clone git@github.com:lvsenlv/c_test.git
+        git clone git@github.com:lvsenlv/sources.git
         ;;
     2 )
-        git clone git@github.com:lvsenlv/sources.git
+        git clone git@github.com:lvsenlv/ubuntu_config.git
         ;;
     3 )
         git clone git@github.com:lvsenlv/vim_config.git
         ;;
     4 )
-        git clone git@github.com:lvsenlv/ubuntu_config.git
+        git clone git@github.com:lvsenlv/c_projects.git
         ;;
     5 )
-        git clone git@github.com:lvsenlv/jz2440.git
+        git clone git@github.com:lvsenlv/c_test.git
         ;;
     6 )
-        git clone git@github.com:lvsenlv/graduation.git
+        git clone git@github.com:lvsenlv/eServer.git
         ;;
     7 )
-        git clone git@github.com:lvsenlv/c_projects.git
+        git clone git@github.com:lvsenlv/easy_chat_server.git
         ;;
     8 )
         git clone git@github.com:lvsenlv/s3c2440a.git
+        ;;
+    9 )
+        git clone git@github.com:lvsenlv/encrypt.git
+        ;;
+    10 )
+        git clone git@github.com:lvsenlv/libparser.git
         ;;
     * )
         ;;
